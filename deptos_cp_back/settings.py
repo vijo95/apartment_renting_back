97fcdd8849f56126a7f0436ec07220bed28dc126
vijo95 <53486442+vijo95@users.noreply.github.com>
2020-10-28 11:30:48 -0300

    Update settings.py

--- deptos_cp_back/settings.py
+++ deptos_cp_back/settings.py
@@ -144,7 +144,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'vicjoaquin070@gmail.com'
-EMAIL_HOST_PASSWORD = 'nnuqytxdwwetznyh'
+EMAIL_HOST_PASSWORD = '**********'
 EMAIL_USE_TLS = True
 
 
